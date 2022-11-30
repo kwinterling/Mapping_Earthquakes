3bcee35b0432a42da39a2344156c07fb0941869d
Karl Winterling <kwinterling@gmail.com>
2022-11-30 11:38:26 -0800

    Added multiple GeoJSON points

--- static/js/logic.js
+++ static/js/logic.js
@@ -23,6 +23,31 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/kwinterling/Mapping_Earthquakes/main/static/data/majorAirports.json";
-NOTE
+// let airportData = "https://raw.githubusercontent.com/kwinterling/Mapping_Earthquakes/main/static/data/majorAirports.json";
+
+// Getting connection reset error
+let airportData = "static/data/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+/* d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
+*/
+
+d3.json(airportData).then(function (data) {
+    console.log(data);
+
+    L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+            console.log(feature);
+            return L.marker(latlng);
+        },
+        onEachFeature: function (feature, layer) {
+            console.log(layer);
+            layer.bindPopup(`<b>Airport Code: ${feature.properties.faa}</b> <hr /> <b>Airport Name: ${feature.properties.name}</b>`);
+        }
+    }).addTo(map);
+});
 
